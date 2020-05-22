2419f65687abfc2a617050d92d6020010a69c686
Fernando Gunawan <flashshadow32@gmail.com>
2020-05-22 20:44:56 +0700

    Change DB connection to vps

--- _config/config.php
+++ _config/config.php
@@ -2,9 +2,11 @@
 $con = new mysqli("localhost", "root", "", "pos");
 // Check connection
 if ($con->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    $con = new mysqli("localhost", "hci", "ProjectHci@2020", "pos");
+    if ($con->connect_error) {
+        die("Connection failed: " . $con->connect_error);
+    }
 }
-
 
 function toRupiah($nominal)
 {
