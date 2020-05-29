fa3de612a9e0f0c5517dfd7acc1a05413638ff9f
Fernando Gunawan <flashshadow32@gmail.com>
2020-05-29 21:45:41 +0700

    Update Increment Decrement, Index Menu

--- header.php
+++ header.php
@@ -3,100 +3,117 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css" />
-    <title>Boss Coffee</title>
-    <link rel="shortcut icon" href="img/logo.jpg" type="image/x-icon">
+	<meta http-equiv=“Pragma” content=”no-cache”>
+	<meta http-equiv=“Expires” content=”-1″>
+	<meta http-equiv=“CACHE-CONTROL” content=”NO-CACHE”>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
+	<script src="https://kit.fontawesome.com/661db8639e.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="css/style.css" />
+	<title>Boss Coffee</title>
+	<link rel="shortcut icon" href="img/logo.jpg" type="image/x-icon">
 </head>
 
 <body class="menu" data-spy="scroll">
-    <header>
-        <nav class="sidebar">
-            <div class="img-center"><img src="img/logo.png" alt="logo" class="img-fluid"></div>
-            <nav class="container" id="menu-center">
-                <input type="text" class="form-control empty" id="myInput" onkeyup="mySearchFunction()" placeholder="&#xF002 Search" />
-                <ul class="list-unstyled">
-                    <li><a class="nav-link" href="#coffee">Coffee</a></li>
-                    <li><a class="nav-link" href="#noncoffee">Non Coffee</a></li>
-                    <li><a class="nav-link" href="#lmeal">Light Meal</a></li>
-                    <li><a class="nav-link" href="#hmeal">Heavy Meal</a></li>
-                    <li><a class="nav-link" href="#dessert">Dessert</a></li>
-                </ul>
-            </nav>
-        </nav>
-        <nav class="navbar fixed-top navbar-light bg-navbar" style="z-index: 1">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">Total</li>
-                <li class="nav-item" id="rupiah">Rp. 0</li>
-            </ul>
+	<header>
+		<nav class="sidebar">
+			<div class="img-center"><img src="img/logo.png" alt="logo" class="img-fluid"></div>
+			<nav class="container" id="menu-center">
+				<input type="text" class="form-control empty" id="myInput" onkeyup="mySearchFunction()" placeholder="&#xF002 Search" />
+				<ul class="list-unstyled">
+					<li><a class="nav-link" href="#coffee">Coffee</a></li>
+					<li><a class="nav-link" href="#noncoffee">Non Coffee</a></li>
+					<li><a class="nav-link" href="#lmeal">Light Meal</a></li>
+					<li><a class="nav-link" href="#hmeal">Heavy Meal</a></li>
+					<li><a class="nav-link" href="#dessert">Dessert</a></li>
+				</ul>
+				<div class="nav-footer muted">
+					Logged in as:
+					<br>
+					Test
+				</div>
+			</nav>
+		</nav>
+		<nav class="navbar fixed-top navbar-light bg-navbar" style="z-index: 1">
+			<a type="button" class="btn button3" href="index.html">
+				<i class="fas fa-backspace"></i> Back </a>
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">Total</li>
+				<li class="nav-item" id="rupiah">Rp. 0</li>
+			</ul>
 			<li class="list-unstyled nav-item">
-			<input type = "button" class = "paybut" value = "confirm" style="background-color:#f14e4e">
+				<input type="button" class="paybut" value="confirm" style="background-color:#f14e4e">
 			</li>
 		</nav>
 		<div id="pay_pop" class="pay_pop">
-            <!-- Modal content -->
-            <div class="payment-content">
+			<!-- Modal content -->
+			<div class="payment-content">
 				<div class="modal-header">
 					<h2>PAYMENT</h2>
 					<span class="close" id="closex">&times;</span>
-                </div>
+				</div>
 				<div class="modal-body">
 					<p>Total: <span id="total">Rp. 0</span></p>
-                    <p>Select Payment Method:</p>
-					<input type = "radio" id = "cash" name = "payment" value = "cash"">
-						<label for = "cash"><tabspace>CASH</tabspace></label>
-					<input type = "radio" id = "ewallet" name = "payment" value = "ewallet">
-						<label for = "ewallet"><tabspace>E-WALLET</tabspace></label>
-					<input type = "radio" id = "edc" name = "payment" value = "edc">
-						<label for = "edc"><tabspace>EDC</tabspace></label>
-					<div class="pay_cash" id = "pay_cash">
+					<p>Select Payment Method:</p>
+					<input type="radio" id="cash" name="payment" value="cash"">
+						<label for = " cash">
+					<tabspace>CASH</tabspace></label>
+					<input type="radio" id="ewallet" name="payment" value="ewallet">
+					<label for="ewallet">
+						<tabspace>E-WALLET</tabspace>
+					</label>
+					<input type="radio" id="edc" name="payment" value="edc">
+					<label for="edc">
+						<tabspace>EDC</tabspace>
+					</label>
+					<div class="pay_cash" id="pay_cash">
 						<tabspace>CASH:</tabspace>
-						<input type="text" id="cashpay">
-						<div align = "right" class = "mt-4 mb-3 confirm" style="display:none"><input type = "button" value = "Confirm Payment" onclick="confirmpay()"></div>
+						<input type="number" id="cashpay">
+						<div align="right" class="mt-4 mb-3 confirm" style="display:none"><input type="button" value="Confirm Payment" onclick="confirmpay()"></div>
 					</div>
 					<div class="pay_ewallet">
 						Select E-Wallet:
-						<ul class = "list-unstyled ewallet_method" id = "id_ewallet">
+						<ul class="list-unstyled ewallet_method" id="id_ewallet">
 							<li>
-								<input type = "radio" id = "ovo" name = "pay">
-								<label for = "pay"><img src = "img/ovo.png">
+								<input type="radio" id="ovo" name="pay">
+								<label for="pay"><img src="img/ovo.png">
 							</li>
 							<li>
-								<input type = "radio" id = "gopay" name = "pay">
-								<label for = "pay"><img src = "img/gopay.png">
+								<input type="radio" id="gopay" name="pay">
+								<label for="pay"><img src="img/gopay.png">
 							</li>
 							<li>
-								<input type = "radio" id = "dana" name = "pay">
-								<label for = "pay"><img src = "img/dana.png">
+								<input type="radio" id="dana" name="pay">
+								<label for="pay"><img src="img/dana.png">
 							</li>
 						</ul>
-						<div class = "pnum" style='display: none'><tabspace>Phone Number:</tabspace>
-						<input type="text" id="pnumber"></div>
-						<div align = "right" class = "mt-4 mb-3 confirm2" style="display:none"><input type = "button" value = "Confirm Payment" onclick="confirmpay2()"></div>
+						<div class="pnum" style='display: none'>
+							<tabspace>Phone Number:</tabspace>
+							<input type="number" id="pnumber">
+						</div>
+						<div align="right" class="mt-4 mb-3 confirm2" style="display:none"><input type="button" value="Confirm Payment" onclick="confirmpay2()"></div>
 					</div>
 					<div class="pay_edc">
 						Select EDC:
-						<ul class = "list-unstyled ewallet_method" id = "id_edc">
+						<ul class="list-unstyled ewallet_method" id="id_edc">
 							<li>
-								<input type = "radio" id = "bca" name = "pay">
-								<label for = "pay">BCA
+								<input type="radio" id="bca" name="pay">
+								<label for="pay">BCA
 							</li>
 							<li>
-								<input type = "radio" id = "mandiri" name = "pay">
-								<label for = "pay">MANDIRI
+								<input type="radio" id="mandiri" name="pay">
+								<label for="pay">MANDIRI
 							</li>
 							<li>
-								<input type = "radio" id = "bni" name = "pay">
-								<label for = "pay">BNI
+								<input type="radio" id="bni" name="pay">
+								<label for="pay">BNI
 							</li>
 						</ul>
-						<div align = "right" class = "mt-4 mb-3 confirm3" style="display:none"><input type = "button" value = "Confirm Payment" onclick="confirmpay2()"></div>
+						<div align="right" class="mt-4 mb-3 confirm3" style="display:none"><input type="button" value="Confirm Payment" onclick="confirmpay2()"></div>
 					</div>
-                </div>
-            </div>
-        </div>
-<!--		<a class = "paybut" style="background-color:#f14e4e">Confirm</a>-->
-    </header>
+				</div>
+			</div>
+		</div>
+		<!--		<a class = "paybut" style="background-color:#f14e4e">Confirm</a>-->
+	</header>
--- menu.php
+++ menu.php
@@ -15,31 +15,65 @@ $data = showDataMenu($con, "menu", "coffee");
                     <!-- Modal content -->
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h2><?php echo $row['nama']; ?></h2>
+                            <h3><?php echo $row['nama']; ?></h3>
                             <span class="close" id="closex">&times;</span>
                         </div>
                         <div class="modal-body">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sollicitudin, tellus ut fermentum pellentesque, magna arcu dapibus nulla, tristique dapibus lorem libero eu lectus. Phasellus mattis magna at orci pellentesque, vel finibus odio tempus. Curabitur sodales ac odio tempus dictum. Aliquam et laoreet erat, sed euismod lectus. Curabitur nunc nulla, suscipit eu risus ac, tincidunt luctus lacus. Etiam vel elementum risus. Praesent imperdiet non tortor vel rutrum. Donec id tortor diam. Sed eu rutrum lectus. Integer leo metus, luctus eget feugiat a, placerat sit amet eros. Cras finibus velit diam, ut aliquet nunc posuere eu. Praesent tempus tellus nisi, eu tincidunt metus imperdiet eu.
-
-                                Vestibulum tempus, risus a ullamcorper vulputate, odio augue eleifend sapien, in pellentesque leo turpis nec erat. Nam sit amet ornare tortor. Nunc dolor enim, suscipit vitae libero at, auctor porttitor odio. Sed accumsan purus id sapien vestibulum, quis placerat urna dapibus. In lacinia, velit eu molestie aliquet, tortor purus vestibulum elit, vitae iaculis nisi enim sollicitudin lacus. Quisque turpis tellus, suscipit sed dignissim id, lobortis id justo. Pellentesque dapibus posuere suscipit. Vivamus tristique volutpat odio eget volutpat. Vivamus at placerat justo, sit amet elementum quam.
-
-                                Etiam a nisi non velit tempus euismod. Praesent semper enim sapien, quis dapibus tortor iaculis et. Pellentesque venenatis odio turpis, at tempor purus vestibulum eget. Etiam vel rhoncus tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla varius enim in lacus auctor vulputate. Mauris elementum euismod diam, maximus eleifend diam porta semper. Maecenas nec est tristique, egestas eros sed, mattis nulla. In in semper lectus, at fringilla velit. Donec interdum, tellus a egestas vehicula, metus elit convallis mauris, quis facilisis lectus dolor at orci. Donec dui quam, pellentesque id lacus et, tempus tincidunt tellus.
-
-                                Aenean nulla nisi, iaculis ut maximus eu, aliquet quis magna. Proin condimentum dapibus lorem, eget condimentum nisl facilisis at. Curabitur lobortis fermentum risus, in feugiat lorem congue lobortis. Suspendisse elementum, magna sed ultricies suscipit, tellus eros imperdiet lorem, in luctus orci urna quis tortor. Integer mollis lacus mauris, vel pellentesque lectus dictum sed. Morbi a porttitor nisl. Maecenas bibendum tempor finibus. Quisque at varius elit. Etiam eu sem quis ante cursus dapibus. Cras nec auctor felis, eu hendrerit velit.
-
-                                Donec ac justo ligula. Sed rutrum interdum nunc id efficitur. Vivamus nec sapien sit amet magna sodales iaculis. Nunc eu sagittis ligula. In vitae sagittis ex. Maecenas sagittis nisi finibus tellus pellentesque commodo. Phasellus vehicula magna et urna porta, ut suscipit leo scelerisque. Integer molestie ut orci ac mattis. Mauris at scelerisque ipsum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin tincidunt vehicula pellentesque. Cras lectus velit, tincidunt eu libero at, fermentum faucibus lectus. Duis sed mauris dolor. Sed tempor, mi non iaculis posuere, ante augue malesuada tellus, a convallis orci justo non lorem.
-
-                                Fusce a enim malesuada, tempus massa sit amet, porta tortor. Fusce hendrerit, dui interdum feugiat dapibus, lorem risus fermentum massa, vel vulputate ligula enim quis nibh. Sed egestas sem feugiat, consectetur risus id, congue enim. Nulla facilisi. Maecenas posuere erat sed aliquam finibus. Sed malesuada tortor vitae est gravida, ut mollis neque mattis. Mauris et libero imperdiet, venenatis dolor vel, rhoncus quam. Aliquam fringilla lorem eget orci rhoncus, non commodo tellus imperdiet.
-
-                                Curabitur tristique purus purus, at consequat risus porta sit amet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi convallis dolor elementum ultricies pulvinar. Quisque vitae imperdiet magna. Fusce lobortis purus suscipit augue egestas dignissim. Sed vel condimentum ligula. Mauris volutpat ligula eget volutpat imperdiet. Cras quis risus sed orci interdum consectetur. Donec eget elit vestibulum libero luctus luctus at in orci. Fusce ligula nibh, congue et faucibus pellentesque, lobortis nec ligula. Mauris augue dolor, feugiat id porttitor eget, sollicitudin at lorem. Morbi urna dui, dapibus sed aliquet nec, dapibus nec erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam pellentesque ante turpis, et pellentesque velit convallis sit amet. Quisque sed fermentum quam, in mollis nisl.
-
-                                Ut sit amet egestas massa. Aliquam in magna vehicula, commodo nisi eu, fermentum justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur erat turpis, aliquam blandit viverra non, consectetur in est. Praesent ultrices sit amet lacus eu efficitur. Suspendisse aliquet neque et pharetra facilisis. Fusce eleifend magna fringilla quam auctor dignissim. Donec fringilla ornare elementum. Nunc lobortis felis id mi vestibulum, sit amet dignissim elit lacinia. Curabitur semper odio velit, et lobortis quam malesuada dictum. In lobortis sapien libero. Morbi pretium dui in leo egestas elementum. Aenean ultrices a quam eu pharetra. Aliquam id accumsan felis. Praesent non elit vel tortor dignissim iaculis.
-
-                                Donec in varius mi. Etiam commodo elementum odio, et euismod neque molestie in. Donec sed imperdiet est. Vivamus ut consequat augue, at posuere risus. Ut hendrerit at nunc eget ultrices. Sed at lectus congue, accumsan mi non, lacinia ex. In molestie in nunc vel interdum. Morbi egestas nulla vitae est tempus aliquet. Nunc lacinia, nulla vel venenatis dapibus, nunc eros suscipit orci, eget sagittis elit mauris quis leo.
-
-                                Ut varius blandit dignissim. Suspendisse potenti. Duis consequat elementum mi, eget faucibus lorem ultrices eu. Maecenas vel dictum nunc. Mauris enim elit, consequat sed accumsan sed, auctor in elit. Nunc nec enim hendrerit, venenatis libero vel, venenatis nisi. Praesent congue rhoncus velit, a pharetra orci semper eget. Vivamus sed commodo lectus. Curabitur quis mauris quis magna mollis consectetur eu suscipit nibh. Pellentesque eget consectetur dui. Vivamus vel elementum nisl, ac feugiat justo. Aliquam at justo lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-
-                            </p>
+                            <div class="container">
+                                <h3>Varieties</h3>
+                                <div class="row">
+                                    <div class="btn-group m-auto" data-toggle="buttons">
+                                        <label class="btn btn-primary btn-lg square">
+                                            <input type="radio" name="variant" id="hot" hidden>HOT
+                                        </label>
+                                        <label class="btn btn-primary btn-lg square">
+                                            <input type="radio" name="variant" id="ice" hidden>ICE
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="container">
+                                <h3>Quantitiy</h3>
+                                <div class="row">
+                                    <div class="m-auto">
+                                        <span><button type="button" class="btn square inc-dec" onclick="less('<?php echo $row['id']; ?>', '<?php echo $row['harga']; ?>')">-</button></span>
+                                        <span id="quantity<?php echo $row['id']; ?>" class="m-4">1</span>
+                                        <span><button type="button" class="btn square inc-dec" onclick="add('<?php echo $row['id']; ?>', '<?php echo $row['harga']; ?>')">+</button></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- <label class="tombol">
+                                <input type="button" onclick="less()">
+                                <span class="square">
+                                    <p>-</p>
+                                </span>
+                            </label>
+                            <div class="tombol">
+                                <p id="quantity">0</p>
+                            </div>
+                            <label class="tombol">
+                                <input type="button" onclick="add()">
+                                <span class="square">
+                                    <p>+</p>
+                                </span>
+                            </label> -->
+                            <div class="container">
+                                <h3>Sales Type</h3>
+                                <div class="row">
+                                    <div class="btn-group m-auto" data-toggle="buttons">
+                                        <label class="btn btn-primary btn-lg square">
+                                            <input type="radio" name="variant" id="hot" hidden>DINE IN
+                                        </label>
+                                        <label class="btn btn-primary btn-lg square">
+                                            <input type="radio" name="variant" id="ice" hidden>TAKE AWAY
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="container">
+                                <h3>Note</h3>
+                                <textarea rows="8" cols="40" class="t-area"></textarea>
+                            </div>
                         </div>
                         <div class="modal-footer">
                             <h3>Modal Footer</h3>
